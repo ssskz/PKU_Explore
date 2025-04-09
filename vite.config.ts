f26bdd84c2e5d4ec386f2ca08ce4e03c288315d4
ssskz <12497590+ssskz@user.noreply.gitee.com>
2025-04-09 06:30:55 +0000

    1
    Signed-off-by: ssskz <12497590+ssskz@user.noreply.gitee.com>

---
+++ vite.config.ts
@@ -0,0 +1,40 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import Components from 'unplugin-vue-components/vite'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/',
+  plugins: [
+    vue(),
+    vueJsx(),
+    vueDevTools(),
+    Components({
+      resolvers: [
+        AntDesignVueResolver({
+          importStyle: false, // css in js
+        }),
+      ],
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        //target: 'http://127.0.0.1:5000',
+        target: 'https://dev.pku.edu.cn/',
+        changeOrigin: true,
+        headers: {},
+      },
+    },
+  },
+})
